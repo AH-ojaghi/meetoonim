2635aac4fbfcc7e6e0fe8e506aac59db889adcfe
fatemehfekrati <fatemefekrati@gmail.com>
2024-07-22 16:54:33 +0330

    CreateLikeAndSetApiPost

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import Comment from "./Comment";
 import { FaRegHeart, FaRegBookmark, FaRegCommentAlt } from "react-icons/fa";
@@ -11,9 +12,32 @@ const cardItems = [
 ];
 
 export default function Content() {
+  const [data, setData] = useState<any[]>([]);
+  
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbWVldG9vbmltLmNvbS9hcGkvdjEvdXNlcnMvbG9naW4iLCJpYXQiOjE3MjEyMTU4MjcsImV4cCI6MTcyNjAxNTgyNywibmJmIjoxNzIxMjE1ODI3LCJqdGkiOiJpN3pPRlRXUnZUN2xkV2hNIiwic3ViIjoiMjE4OTIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.HQtTBA6rBRSM_IwglUvM6JGdFF4VQrAuyRkcvWa7d5I");
+    
+   
+    
+    fetch("https://meetoonim.com/api/v1/posts/?page=1", {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.data)
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div className="w-full md:w-[59%] lgs:w-[64%] md:px-3 lg:w-[46.6%] xl:w-[53%]">
-      <Card
+      {
+        data.map((card) => <Card description={card.description} />)
+      }
+      {/* <Card
         logo="/vite.svg"
         companyName="کارگزاری مفید"
         time="11 ماه پیش"
@@ -58,7 +82,7 @@ export default function Content() {
           time="6 ماه پیش"
           text="خیلی هم عالی"
         />
-      </Card>
+      </Card> */}
     </div>
   );
 }
