86d304baf69462016e35e5a0d5a7b8cc772b091d
AH-ojaghi <ojaghiamirhossein325@gmail.com>
2024-07-23 16:58:05 +0330

    create slider for image content

--- src/Utils/Slider.tsx
+++ src/Utils/Slider.tsx
@@ -1,41 +1,41 @@
-import { useRef, useEffect } from "react";
-import { register } from "swiper/element/bundle";
-register();
-// //
+import { RxDot } from "react-icons/rx";
+import { RxDotFilled } from "react-icons/rx";
 
-const Slider = () => {
-  const swiperElRef = useRef(null);
+//
 
-  useEffect(() => {
-    // listen for Swiper events using addEventListener
-    swiperElRef.current.addEventListener("swiperprogress", (e) => {
-      const [swiper, progress] = e.detail;
-      console.log(progress);
-    });
-
-    swiperElRef.current.addEventListener("swiperslidechange", (e) => {
-      console.log("slide changed");
-    });
-  }, []);
-
+const sliderItems = [
+  {
+    src: "https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp",
+  },
+  {
+    src: "https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp",
+  },
+  {
+    src: "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+  },
+];
+function Slider() {
   return (
-    <swiper-container
-      ref={swiperElRef}
-      slides-per-view="3"
-      navigation="true"
-      pagination="true"
-    >
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide e</swiper-slide>
-      <swiper-slide>Slide b</swiper-slide>
-      <swiper-slide>Slide c</swiper-slide>
-      <swiper-slide>Slide d</swiper-slide>
-      <swiper-slide>Slide f</swiper-slide>
-      <swiper-slide>Slide g</swiper-slide>
-      ...
-    </swiper-container>
+    <>
+      <div className="container">
+        <div className="slider">
+          {sliderItems.map((item) => {
+            return (
+              <>
+                <div className="sliderItem">
+                  <img src={item.src} alt="" />
+                </div>
+              </>
+            );
+          })}
+          <div className="current-slide">
+            {/* <button>{icon}</button> */}
+            <RxDot />
+          </div>
+        </div>
+      </div>
+    </>
   );
-};
+}
+
 export default Slider;
