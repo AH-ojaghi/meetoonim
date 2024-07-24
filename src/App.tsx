b13d9c0f57fd5209c6ffa12fdeccc4a8a8ff27e8
AH-ojaghi <ojaghiamirhossein325@gmail.com>
2024-07-24 16:58:56 +0330

    modal is completed, optimization of card component, connection of each card action to api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import LandingPage from "./Pages/LandingPage";
 import Header from "./Components/Header/Header";
 import BottomMenu from "./Components/BottomMenu/BottomMenu";
 import MobileAddMenu from "../src/Components/MobileAddMenu";
-import Modal from "./Utils/Modal";
+// import Modal from "./Utils/Modal";
+
 
 export default function App() {
   return (
