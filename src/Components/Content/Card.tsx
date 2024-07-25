66f1bb194f078453cc481a45c5802a6d82756180
fatemehfekrati <fatemefekrati@gmail.com>
2024-07-25 12:07:51 +0330

    ModificationComponentCardAnd Comments

--- src/Components/Content/Card.tsx
+++ src/Components/Content/Card.tsx
@@ -7,8 +7,7 @@ import { FaRegBookmark, FaRegHeart } from "react-icons/fa6";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { MdOutlineIosShare } from "react-icons/md";
 import { VscAccount } from "react-icons/vsc";
-import { TbPointFilled } from "react-icons/tb";
-import { BsThreeDots } from "react-icons/bs";
+
 function Action({ img, text }: { img: React.ReactNode; text: string }) {
   const [color, setColor] = useState("black");
 
@@ -39,7 +38,7 @@ const cardItems = [
   },
   { img: <FaRegBookmark className="text-[16px]" />, label: "ذخیره" },
 ];
-const MenuComment = [{ label: "حذف " }, { label: "ویرایش" }];
+
 const actionsList = [
   { name: "اشتراک گذاری" },
   { name: "بلاک کردن" },
@@ -48,37 +47,31 @@ const actionsList = [
 ];
 
 export default function Card({
+  thumbnail,
   description,
-  created_at,
   f_name,
   l_name,
   open_to_image,
-  thumbnail,
   comments_count,
-  likes_count,
-  comment,
-  likes_countComments,
-  open_to_imageComments,
-  id_user,
-  id_comments,
+  created_at
+ 
 }: {
   description: string;
-  created_at: string;
+  created_at:string
   f_name: string;
   l_name: string;
   open_to_image: string;
   thumbnail: string;
   comments_count: string;
-  likes_count: number;
-  comment: string;
-  likes_countComments: number;
-  open_to_imageComments: string;
-  id_user: number;
-  id_comments: number[];
+
+  
+
+  
+
 }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [favoriteIndex, setFavoriteIndex] = useState<null | number>(null);
-  const [open, setOpen] = useState(false);
+  
   const toggleFavorite = (index: null) => {
     setFavoriteIndex((prevIndex) => (prevIndex === index ? null : index));
   };
@@ -88,9 +81,7 @@ export default function Card({
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
-  const toggleDropdownnn = () => {
-    setOpen(!open);
-  };
+
   const handleClickOutside = (event: Event) => {
     if (!(event.target as HTMLElement).closest(".dropdown")) {
       setDropdownOpen(false);
@@ -105,7 +96,7 @@ export default function Card({
   }, []);
 
   return (
-    <div className="mb-2 md:rounded-xl md:mb-4 bg-white border border-slate-200">
+    <div className="mb-2 md:mb-4 bg-white ">
       <div className="px-[17.3px] py-3 w-full flex justify-between">
         <div className="flex gap-3">
           {open_to_image ? (
@@ -216,93 +207,6 @@ export default function Card({
           );
         })}
       </div>
-
-      {comment && likes_countComments && created_at && f_name && l_name && (
-        <>
-          <div className="w-full flex justify-center gap-3 items-center mb-2">
-            {open_to_image ? (
-              <img
-                src={
-                  open_to_imageComments
-                    ? `https://meetoonim.com/${open_to_imageComments}`
-                    : "/"
-                }
-                className="bg-blue-400 w-10 h-10 rounded-full"
-              />
-            ) : (
-              <VscAccount />
-            )}
-
-            <div className="w-[500px] h-[60px]  rounded-[10px] flex gap-[270px]  bg-[#f5f5f5]">
-              <div className="flex  flex-col mr-2  justify-center">
-                <div className="flex items-center justify-center">
-                  <p className="text-black/90 flex gap-1 text-[#444444] items-center justify-center">
-                    {f_name}
-                    <p>{l_name}</p>
-                    <div>
-                      <TbPointFilled className="text-[#9e9e9e] w-3/4 h-3/4" />
-                    </div>
-                  </p>
-                  <p className="text-[10px] text-[#9e9ec1] font-B">
-                    {created_at}
-                  </p>
-                </div>
-
-                <div className="flex">
-                  <p className="text-[11px]">{comment}</p>
-                </div>
-              </div>
-              <div
-                onClick={() => toggleFavorite}
-                className="flex items-center  mb-2 gap-3"
-              >
-                <p className="font-B text-[#9e9ec1]">{likes_countComments}</p>
-                <div>
-                  {favoriteIndex ? (
-                    <FcLike className="w-3" />
-                  ) : (
-                    <FaRegHeart className="w-3" />
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-          {id_comments.includes(id_user) ? (
-            <div className="flex gap-[450px]">
-              <div className="flex w-[120px] text-[#9797bd] text-sm justify-center mb-1">
-                <button>پاسخ</button>
-              </div>
-              <div className="dropdown relative  inline-block">
-                <button
-                  onClick={() => {
-                    toggleDropdownnn();
-                  }}
-                  className="dropbtn rounded focus:outline-none text-start"
-                >
-                  <BsThreeDots />
-                </button>
-                <div
-                  id="myDropdown"
-                  className={`dropdown z-10 absolute w-36 mt-2 left-0 bg-white border border-gray-200 rounded shadow-lg transition-all duration-500 ${
-                    open ? "max-h-60 opacity-100" : "max-h-0 opacity-0"
-                  } overflow-hidden`}
-                >
-                  {MenuComment.map((item, index) => (
-                    <a
-                      key={index}
-                      className="block px-4 py-2 text-black hover:bg-gray-200"
-                    >
-                      {item.label}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </>
-      )}
     </div>
   );
 }
